49e8a61b33306fb4d85f67399358a862ecbda15e
bhatsanjana <sanjanabhat0412@gmail.com>
2024-04-27 19:34:32 +0530

    Initial commit-version

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+
+import time
+
+def main():
+    while True:
+        print("Container is running...")
+        time.sleep(5)  # Sleep for 5 seconds before printing again
+
+if __name__ == "__main__":
+    main()
